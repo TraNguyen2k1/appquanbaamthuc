e6d5e972ecfc86324491591005861bde1533746e
ThanhTra <ntra278@gmail.com>
2022-11-12 09:26:29 +0700

    do an chuyen nghanh

---
+++ app/src/main/java/vn/edu/stu/app_quan_ba_am_thuc/MainActivity.java
@@ -0,0 +1,14 @@
+package vn.edu.stu.app_quan_ba_am_thuc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
